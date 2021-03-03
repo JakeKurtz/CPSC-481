026e01d6a2fed440e2ef954dc7f153afef7a7977
JakeKurtz <jake.kurtz@ucalgary.ca>
2021-03-02 18:02:10 -0700

    Added buttons.

--- CPSC-481/MainWindow.xaml.cs
+++ CPSC-481/MainWindow.xaml.cs
@@ -21,30 +21,63 @@ namespace CPSC_481
     public partial class MainWindow : Window
     {
 
-        public MainWindow()
+		public MainWindow()
         {
             InitializeComponent();
 
 			List<Item> orderCurrent = new List<Item>();
 			List<Item> orderHistory = new List<Item>();
 
-			List<Addon> _Addons = new List<Addon>();
+			List<Addon> order1_addons = new List<Addon>();
+			List<Addon> order2_addons = new List<Addon>();
+			List<Addon> order3_addons = new List<Addon>();
+			List<Addon> order4_addons = new List<Addon>();
 
-			_Addons.Add(new Addon() { Name = "addon_0", Cost = 0.50f, Quantity = 1 });
-			_Addons.Add(new Addon() { Name = "addon_1", Cost = 1.49f, Quantity = 1 });
+			List<String> order1_options = new List<String>();
+			List<String> order2_options = new List<String>();
+			List<String> order3_options = new List<String>();
+			List<String> order4_options = new List<String>();
 
-			orderCurrent.Add(new Item()  {Main = "main_0", Addons = _Addons, Cost = 1.99f, Quantity = 1 });
-			orderCurrent.Add(new Item() { Main = "main_1", Addons = _Addons, Cost = 1.99f, Quantity = 1 });
+			order1_options.Add("Blackened Chicken");
+			order1_addons.Add(new Addon() { Name = "Artisan Garlic Bread", Cost = 2.00f, Quantity = 1 });
 
-			orderHistory.Add(new Item() { Main = "main_2", Addons = _Addons, Cost = 1.99f, Quantity = 1 });
-			orderHistory.Add(new Item() { Main = "main_3", Addons = _Addons, Cost = 1.99f, Quantity = 1 });
+			order2_options.Add("3 oz Salmon");
+			order2_addons.Add(new Addon() { Name = "Extra Sauce", Cost = 1.5f, Quantity = 1 });
+
+			order3_options.Add("Fries");
+			order3_options.Add("Lobster Tail");
+			order3_addons.Add(new Addon() { Name = "Extra Prawns", Cost = 10.00f, Quantity = 1 });
+
+			order4_options.Add("Salt and pepper");
+			order4_addons.Add(new Addon() { Name = "Ranch", Cost = 2.00f, Quantity = 2 });
+
+			orderCurrent.Add(new Item() { Name = "Chicken Fettuccine Alfredo", Addons = order1_addons, Options = BuildOptions(order1_options), Cost = 23.00f, Quantity = 1 });
+			orderCurrent.Add(new Item() { Name = "Salmon Zen Bowl", Addons = order2_addons, Cost = 28.50f, Options = BuildOptions(order2_options), Quantity = 1 });
+			orderCurrent.Add(new Item() { Name = "Surf and Turf 6 oz Filet", Addons = order3_addons, Options = BuildOptions(order3_options), Cost = 48.00f, Quantity = 1 });
+
+			orderHistory.Add(new Item() { Name = "Wings hot", Addons = order4_addons, Options = BuildOptions(order4_options), Cost = 33.00f, Quantity = 2 });
+			orderHistory.Add(new Item() { Name = "Galic Fries", Addons = new List<Addon>(), Options = BuildOptions(new List<string>()), Cost = 8.00f, Quantity = 1 });
 
 			List<Order> orders = new List<Order>();
 			orders.Add(new Order() { Name = "Current Order", Items = orderCurrent });
 			orders.Add(new Order() { Name = "Order History", Items = orderHistory });
-			orderList.ItemsSource = orders;
+			_orderCurrent.ItemsSource = orderCurrent;
+			_orderHistory.ItemsSource = orderHistory;
 		}
-    }
+
+		private String BuildOptions(List<String> options)
+		{
+			if (options.Count == 0) return "";
+
+			var output = "with ";
+			for (int i = 0; i < options.Count; i++)
+			{
+				output += options[i];
+				if (i != options.Count-1) output += " and ";
+			}
+			return output;
+		}
+	}
 }
 
 public class Addon
@@ -56,8 +89,9 @@ public class Addon
 
 public class Item
 {
-	public string Main { get; set; }
+	public string Name { get; set; }
 	public List<Addon> Addons { get; set; }
+	public String Options { get; set; }
 	public float Cost { get; set; }
 	public int Quantity { get; set; }
 }
@@ -65,5 +99,4 @@ public class Order
 {
 	public string Name { get; set; }
 	public List<Item> Items { get; set; }
-
 }
