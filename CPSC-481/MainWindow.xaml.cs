fb31aacf1be13e43d613278eac7581b91298555f
JakeKurtz <jake.kurtz@ucalgary.ca>
2021-03-03 10:10:31 -0700

    Added order time stamps and state. Fixed some other issues.

--- CPSC-481/MainWindow.xaml.cs
+++ CPSC-481/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,45 +27,71 @@ namespace CPSC_481
             InitializeComponent();
 
 			List<Item> orderCurrent = new List<Item>();
-			List<Item> orderHistory = new List<Item>();
 
-			List<Addon> order1_addons = new List<Addon>();
-			List<Addon> order2_addons = new List<Addon>();
-			List<Addon> order3_addons = new List<Addon>();
-			List<Addon> order4_addons = new List<Addon>();
+			List<Addon> item1_addons = new List<Addon>();
+			List<Addon> item2_addons = new List<Addon>();
+			List<Addon> item3_addons = new List<Addon>();
 
-			List<String> order1_options = new List<String>();
-			List<String> order2_options = new List<String>();
-			List<String> order3_options = new List<String>();
-			List<String> order4_options = new List<String>();
+			List<String> item1_options = new List<String>();
+			List<String> item2_options = new List<String>();
+			List<String> item3_options = new List<String>();
 
-			order1_options.Add("Blackened Chicken");
-			order1_addons.Add(new Addon() { Name = "Artisan Garlic Bread", Cost = 2.00f, Quantity = 1 });
+			// ---- Current Order Data ---- //
 
-			order2_options.Add("3 oz Salmon");
-			order2_addons.Add(new Addon() { Name = "Extra Sauce", Cost = 1.5f, Quantity = 1 });
+			item1_options.Add("Blackened Chicken");
+			item1_addons.Add(new Addon() { Name = "Artisan Garlic Bread", Cost = 2.00f, Quantity = 1 });
 
-			order3_options.Add("Fries");
-			order3_options.Add("Lobster Tail");
-			order3_addons.Add(new Addon() { Name = "Extra Prawns", Cost = 10.00f, Quantity = 1 });
+			item2_options.Add("3 oz Salmon");
+			item2_addons.Add(new Addon() { Name = "Extra Sauce", Cost = 1.5f, Quantity = 1 });
 
-			order4_options.Add("Salt and pepper");
-			order4_addons.Add(new Addon() { Name = "Ranch", Cost = 2.00f, Quantity = 2 });
+			item3_options.Add("Fries");
+			item3_options.Add("Lobster Tail");
+			item3_addons.Add(new Addon() { Name = "Extra Prawns", Cost = 10.00f, Quantity = 1 });
 
-			orderCurrent.Add(new Item() { Name = "Chicken Fettuccine Alfredo", Addons = order1_addons, Options = BuildOptions(order1_options), Cost = 23.00f, Quantity = 1 });
-			orderCurrent.Add(new Item() { Name = "Salmon Zen Bowl", Addons = order2_addons, Cost = 28.50f, Options = BuildOptions(order2_options), Quantity = 1 });
-			orderCurrent.Add(new Item() { Name = "Surf and Turf 6 oz Filet", Addons = order3_addons, Options = BuildOptions(order3_options), Cost = 48.00f, Quantity = 1 });
+			orderCurrent.Add(new Item() { Name = "Chicken Fettuccine Alfredo", Addons = item1_addons, Options = BuildOptions(item1_options), Cost = 23.00f, Quantity = 1 });
+			orderCurrent.Add(new Item() { Name = "Salmon Zen Bowl", Addons = item2_addons, Cost = 28.50f, Options = BuildOptions(item2_options), Quantity = 1 });
+			orderCurrent.Add(new Item() { Name = "Surf and Turf 6 oz Filet", Addons = item3_addons, Options = BuildOptions(item3_options), Cost = 48.00f, Quantity = 1 });
 
-			orderHistory.Add(new Item() { Name = "Wings hot", Addons = order4_addons, Options = BuildOptions(order4_options), Cost = 33.00f, Quantity = 2 });
-			orderHistory.Add(new Item() { Name = "Galic Fries", Addons = new List<Addon>(), Options = BuildOptions(new List<string>()), Cost = 8.00f, Quantity = 1 });
+			// ---- Order History Data ---- //
 
-			List<Order> orders = new List<Order>();
-			orders.Add(new Order() { Name = "Current Order", Items = orderCurrent });
-			orders.Add(new Order() { Name = "Order History", Items = orderHistory });
+			List<Order> orderHistory = new List<Order>();
+			List<Item> order4_items = new List<Item>();
+			List<String> order4_item1_options = new List<String>();
+			List<Addon> order4_item1_addons = new List<Addon>();
+
+			List<Item> order5_items = new List<Item>();
+
+			List<String> order5_item1_options = new List<String>();
+			List<Addon> order5_item1_addons = new List<Addon>();
+
+			List<String> order5_item2_options = new List<String>();
+			List<Addon> order5_item2_addons = new List<Addon>();
+
+			order4_item1_options.Add("Salt and pepper");
+			order4_item1_addons.Add(new Addon() { Name = "Ranch", Cost = 2.00f, Quantity = 2 });
+			order4_items.Add(new Item() { Name = "Wings hot", Addons = order4_item1_addons, Options = BuildOptions(order4_item1_options), Cost = 33.00f, Quantity = 2 });
+
+			order4_items.Add(new Item() { Name = "Galic Fries", Addons = new List<Addon>(), Options = BuildOptions(new List<string>()), Cost = 8.00f, Quantity = 1 });
+
+			order5_item1_options.Add("Fries");
+			order5_item1_options.Add("Substituted lettuce for bun");
+			order5_items.Add(new Item() { Name = "Vegan Burger", Addons = order5_item1_addons, Options = BuildOptions(order5_item1_options), Cost = 21.7f, Quantity = 1 });
+
+			order5_item2_addons.Add(new Addon() { Name = "Chicken", Cost = 6.00f, Quantity = 1 });
+			order5_items.Add(new Item() { Name = "Quinoa and Avocado Powerbowl", Addons = order5_item2_addons, Options = BuildOptions(order5_item2_options), Cost = 16.7f, Quantity = 1 });
+
+			Order order4 = new Order() { State = OrderState.Delivered, Time = DateTime.Now, Items = order4_items};
+			Order order5 = new Order() { State = OrderState.Processing, Time = DateTime.Now, Items = order5_items};
+
+			orderHistory.Add(order4);
+			orderHistory.Add(order5);
+
+			// ---- Send it ---- //
+
 			_orderCurrent.ItemsSource = orderCurrent;
 			_orderHistory.ItemsSource = orderHistory;
 		}
-
+		// Builds a string out of the selected options.
 		private String BuildOptions(List<String> options)
 		{
 			if (options.Count == 0) return "";
@@ -78,6 +105,32 @@ namespace CPSC_481
 			return output;
 		}
 	}
+
+	public class StateColorSelector : IValueConverter
+	{
+		private readonly Color _processingColor = Colors.RoyalBlue;
+		private readonly Color _deliveredColor = Colors.OliveDrab;
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is OrderState)
+			{
+				return (OrderState)value == OrderState.Processing ? new SolidColorBrush(_processingColor) : new SolidColorBrush(_deliveredColor);
+			}
+
+			return Binding.DoNothing;
+		}
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
+
+public enum OrderState
+{
+	Delivered,
+	Processing
 }
 
 public class Addon
@@ -95,8 +148,10 @@ public class Item
 	public float Cost { get; set; }
 	public int Quantity { get; set; }
 }
+
 public class Order
 {
-	public string Name { get; set; }
+	public OrderState State { get; set; }
+	public DateTime Time { get; set; }
 	public List<Item> Items { get; set; }
 }
