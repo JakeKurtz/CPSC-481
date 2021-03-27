660ff99c3376f68d3bcfb527f116fb4e30c61706
ckhuu <ckhuu@cs.ucalgary.ca>
2021-03-26 19:04:44 -0600

    Created method to darken button background, indicating the currently selected menu

--- CPSC-481/FoodMenu.xaml.cs
+++ CPSC-481/FoodMenu.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 
-
 namespace CPSC_481
 {
 	/// <summary>
@@ -31,12 +30,14 @@ namespace CPSC_481
 			// pass the appetizer list into the constructor of the AppetizerPage
 			Food.Content = new AppetizerPage(menuLists.AppetizerList);
 			menuTitle.Text = "Appetizers";
+			btnSelected(1);
 		}
 
 		private void specialsClick(object sender, RoutedEventArgs e)
 		{
 			Food.Content = new SpecialsPage(menuLists.SpecialsList);
 			menuTitle.Text = "Today's Specials";
+			btnSelected(0);
 		}
 
 		private void appetizerClick(object sender, RoutedEventArgs e)
@@ -44,18 +45,53 @@ namespace CPSC_481
 			// pass the appetizer list into the constructor of the AppetizerPage
 			Food.Content = new AppetizerPage(menuLists.AppetizerList);
 			menuTitle.Text = "Appetizers";
+			btnSelected(1);
 		}
 
 		private void mainClick(object sender, RoutedEventArgs e)
 		{
 			Food.Content = new MainsPage(menuLists.MainList);
 			menuTitle.Text = "Main Dishes";
+			btnSelected(2);
 		}
 
 		private void sidesClick(object sender, RoutedEventArgs e)
 		{
 			Food.Content = new SidesPage(menuLists.SideList);
 			menuTitle.Text = "Sides";
+			btnSelected(3);
+		}
+
+		// switches the background colour of buttons based on the menu that's selected
+		private void btnSelected(int n) {
+			if (n == 0)
+			{
+				specialsFoodBtn.Background = Brushes.LightGray;
+				appetizersBtn.Background = Brushes.White;
+				mainBtn.Background = Brushes.White;
+				sidesBtn.Background = Brushes.White;
+			}
+			else if (n == 1)
+			{
+				specialsFoodBtn.Background = Brushes.White;
+				appetizersBtn.Background = Brushes.LightGray;
+				mainBtn.Background = Brushes.White;
+				sidesBtn.Background = Brushes.White;
+			}
+			else if (n == 2)
+			{
+				specialsFoodBtn.Background = Brushes.White;
+				appetizersBtn.Background = Brushes.White;
+				mainBtn.Background = Brushes.LightGray;
+				sidesBtn.Background = Brushes.White;
+			}
+			else
+			{
+				specialsFoodBtn.Background = Brushes.White;
+				appetizersBtn.Background = Brushes.White;
+				mainBtn.Background = Brushes.White;
+				sidesBtn.Background = Brushes.LightGray;
+			}
 		}
 	}
 }
